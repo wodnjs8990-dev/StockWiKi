cc837ebb10e3b532ca6b0be6212580dcf190630e
wodnjs8990-dev <wodnjs8990@gmail.com>
2026-04-17 16:22:29 +0900

    redesign: calculator accordion layout for all devices

--- components/StockWiki.tsx
+++ components/StockWiki.tsx
@@ -174,8 +174,6 @@ export default function StockWiki() {
           <CalculatorView
             selectedCalc={selectedCalc}
             setSelectedCalc={setSelectedCalc}
-            sidebarOpen={sidebarOpen}
-            setSidebarOpen={setSidebarOpen}
           />
         )}
       </main>
